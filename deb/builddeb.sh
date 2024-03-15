50dfb757a420f6a3ec021c04c14bad01856d8af3
Alexander Bekrenev <alexander.bekrenev@gmail.com>
2024-03-15 20:20:38 +0300

    Deb package naming

--- deb/builddeb.sh
+++ deb/builddeb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 VERSION="2.1.128.2"
 ARCH="amd64"
+PACKAGE="seqfwd"
 
 if [[ $1 =~ ^(http:\/\/|https:\/\/)[a-z0-9\-]+(\.[a-z0-9\-]+)*(:[0-9]{1,5})?(\/.*)?$ ]]
 then
@@ -13,10 +14,10 @@ if [ -z "$VERSION" ]; then
 fi
 if [[ -n $URL ]]
 then
-    VERSION="custom-$VERSION"
+    PACKAGE="$PACKAGE-custom"
 fi
 
-echo "Build DEB-package for seqfwd version $VERSION"
+echo "Build DEB-package for $PACKAGE version $VERSION"
 
 rm -rf ./publish.seqfwd
 
@@ -30,27 +31,29 @@ else
   echo "Publish directory is ready"
 fi
 
-rm -rf seqfwd-$VERSION-$ARCH
-mkdir -p seqfwd-$VERSION-$ARCH/usr/lib/seqfwd
-cp -r ./publish.seqfwd/* seqfwd-$VERSION-$ARCH/usr/lib/seqfwd
-cp -r seqfwd.template/* seqfwd-$VERSION-$ARCH
+rm -rf $PACKAGE-$VERSION-$ARCH
+mkdir -p $PACKAGE-$VERSION-$ARCH/usr/lib/seqfwd
+cp -r ./publish.seqfwd/* $PACKAGE-$VERSION-$ARCH/usr/lib/seqfwd
+cp -r seqfwd.template/* $PACKAGE-$VERSION-$ARCH
 #update version in control file
-sed -i "s/Version:X\.X/Version:$VERSION/g" seqfwd-$VERSION-$ARCH/DEBIAN/control
+ sed -i "s/Version:X\.X/Version:$VERSION/g" $PACKAGE-$VERSION-$ARCH/DEBIAN/control
 if [[ -n $URL ]]
 then
-  echo "Set server url to $URL"
-  sed -i "s|\-\-url=http:\/\/localhost:5341|--url=$URL|gm" seqfwd-$VERSION-$ARCH//etc/systemd/system/seqfwd.service.d/override.conf
+ echo "Change package name to $PACKAGE"
+ sed -i "s/Package:seqfwd/Package:$PACKAGE/g" $PACKAGE-$VERSION-$ARCH/DEBIAN/control
+ echo "Set server url to $URL"
+  sed -i "s|\-\-url=http:\/\/localhost:5341|--url=$URL|gm" $PACKAGE-$VERSION-$ARCH//etc/systemd/system/seqfwd.service.d/override.conf
 fi 
-chmod 644 "seqfwd-$VERSION-$ARCH/lib/systemd/system/seqfwd.service"
+chmod 644 "$PACKAGE-$VERSION-$ARCH/lib/systemd/system/seqfwd.service"
 
 
-if ! dpkg-deb --root-owner-group -v --build seqfwd-$VERSION-$ARCH
+if ! dpkg-deb --root-owner-group -v --build $PACKAGE-$VERSION-$ARCH
 then
     exit 3
 fi
 echo "Remove build directory"
-rm -rf seqfwd-$VERSION-$ARCH
+rm -rf $PACKAGE-$VERSION-$ARCH
 
-echo "DEB-package for seqfwd version $VERSION: seqfwd-$VERSION-$ARCH.deb"
+echo "DEB-package for seqfwd version $VERSION: $PACKAGE-$VERSION-$ARCH.deb"
 exit 0
 
